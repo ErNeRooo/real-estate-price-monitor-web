09a29b8379a9bb1e80de4a22524086ac0b63e9d7
ErNeRooo <erneroooooo@gmail.com>
2024-04-25 18:16:54 +0200

    add SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import styles from "./App.module.sass";
+import { SideBar } from "./components/sideBar/SideBar";
 
 function App() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.mainBar}></div>
-        <div className={styles.sideBar}></div>
+        <SideBar></SideBar>
         <div className={styles.clearBoth}></div>
       </div>
     </div>
---
+++ src/components/sideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+import styles from "./SideBar.module.sass";
+
+export const SideBar = () => {
+  return (
+    <>
+      <div className={styles.sideBar}></div>
+    </>
+  );
+};
